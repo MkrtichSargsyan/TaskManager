c6f06f3154454f92b336f75359038bfd93c8d8e6
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-17 20:08:09 +0400

    change html extencions to hbs extencion

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-unresolved */
-
-module.exports = {
-  plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer'),
-  ],
-};
