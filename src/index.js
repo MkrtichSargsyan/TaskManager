6636d015302f5e0775b15e1fdb350f5662b9b7b0
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-17 21:34:29 +0400

    Create footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import "../node_modules/normalize.css";
 import "./css/style.css";
 import "./css/customStyles.css";
 import logo from "./images/logo.png";
+import mikeImg from './images/mike.jpeg'
+import emanImg from './images/eman.jpeg'
 
 const logoElm = document.getElementById("logo");
 logoElm.setAttribute("src", logo);
+
+const mike = document.getElementById('mike')
+mike.setAttribute('src',mikeImg)
+const eman = document.getElementById('eman')
+eman.setAttribute('src',emanImg)
