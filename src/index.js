91394f81ac1813151c27fc90a75570d176c9c4ea
Emas <emas.dev@gmail.com>
2021-02-18 10:08:17 -0600

    Add modal to add new goal

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ generateImages
 import { generateProjects, addProjectsToNode } from "./controllers/projectsController";
 
 const projectsElm = document.getElementById("projects-container");
+const addGoalBtn = document.getElementById("add-goal-btn");
+const closeModal = document.getElementsByClassName("close-modal")
+const modal = document.getElementById("modal");
+
 
 let projects = generateProjects();
 addProjectsToNode(projects, projectsElm);
+
+addGoalBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+})
+
+for (let index = 0; index < closeModal.length; index++) {
+  const element = closeModal[index];
+  element.addEventListener("click", () =>{
+    modal.classList.add("hidden");
+  })
+  
+}
